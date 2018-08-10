0416463d45baa8ab7f67e6dcea3bb92d7215a9ad
Guillaume Gouin <guillaume.gouin@ensta-paristech.fr>
2018-08-10 07:40:13 +0200

    First implementation of a nice bootstrap form

--- apps/annuaire/form.py
+++ apps/annuaire/form.py
@@ -8,10 +8,28 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('photo', 'bio', 'promo', 'gap_year', 'miscellaneous',)
 
+    #  We define here the bootstrap class to use. First we put the default
+    #  form-control class for all the widget, and then we modify the
+    #  specific fields like file or checkbox
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+        self.fields['photo'].widget.attrs.update({'class':'form-control-file'})
+        self.fields['gap_year'].widget.attrs.update({'class': 'form-check mt-2'})
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email',)
+
+    #  See the comment in ProfileForm
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs['readonly'] = True
+        
 
 class MemberForm(forms.ModelForm):
     class Meta:
