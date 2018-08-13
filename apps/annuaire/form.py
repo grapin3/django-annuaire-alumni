174bbe71614c7132907c2057c609b71cd14ec783
Guillaume Gouin <guillaume.gouin@ensta-paristech.fr>
2018-08-13 18:36:21 +0200

    Add the support for the gender and localisation field

--- apps/annuaire/form.py
+++ apps/annuaire/form.py
@@ -7,11 +7,31 @@ from .models import Profile, Member
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('photo', 'bio', 'promo', 'gap_year', 'miscellaneous',)
+        fields = ('photo', 'bio', 'promo', 'city', 'region', 'country',
+                'gender', 'gap_year', 'miscellaneous',)
         help_texts = {
                 'promo': "Année de votre diplome",
                 }
 
+    def clean(self):
+        cleaned_data = super(ProfileEditForm, self).clean()
+        city = cleaned_data.get('city')
+        region = cleaned_data.get('region')
+        country = cleaned_data.get('country')
+
+        if city:
+            city = city.split(", ")
+            cleaned_data["country"] = None
+            cleaned_data["region"] = None
+            if len(city)>=1:
+                cleaned_data["country"] = city[-1]
+            if len(city)>=2:
+                cleaned_data["region"] = city[-2]
+            if len(city)>=3:
+                cleaned_data["city"] = city[-3]
+            else:
+                cleaned_data["city"] = None
+            
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
